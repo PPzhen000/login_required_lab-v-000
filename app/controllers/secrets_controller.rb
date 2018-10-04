38fe727490c9e21b326c94eec0954b48fd9de326
Ariel Zhen <zhenpingping@live.com>
2018-10-04 21:18:09 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,11 +2,7 @@ class SecretsController < ApplicationController
   before_action :require_login
   
   def show 
-    if !current_user 
-      redirect_to '/login' 
-    else 
-      render 'show'
-    end 
+
   end 
   
   private
